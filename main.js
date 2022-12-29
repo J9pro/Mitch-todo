0eee69f6bc7930b286f66aa3ebed2ce0b41d1fc3
J9pro <janine239@gmail.com>
2022-12-29 16:58:27 +0100

    first

---
+++ main.js
@@ -0,0 +1,26 @@
+const input = document.getElementById('btn')
+
+
+//Event Listeners
+document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter" && (document.activeElement.tagName.toLowerCase() !== 'button')) {
+        addInput();
+
+    }
+});
+
+input.addEventListener("click", addInput);
+
+
+//Functions
+function addInput() {
+    let info = document.getElementById('input').value
+    const li = document.createElement('li')
+    li.textContent = info
+    document.querySelector('ul').appendChild(li)
+    clear()
+}
+
+function clear() {
+    document.querySelectorAll('input').value = " "
+}
